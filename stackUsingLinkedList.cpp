f681d4fd5e785c90d2236fde52d8b9da55564b53
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-22 10:30:34 +0700

    add case 1

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -95,6 +95,10 @@ int main()
 
     switch (choice)
     {
-        
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); // push the entered value onto thr stack
+            break;
     }
 }
